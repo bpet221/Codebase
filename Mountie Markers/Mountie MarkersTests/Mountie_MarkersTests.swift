66e3a8d1f9d2bf128e0b0df76b49c9fb2f6f644b
Blake Petipas <blakepetipas221@gmail.com>
2025-07-16 11:22:47 -0400

    Fix: Flattened Mountie Markers folder (was submodule)

---
+++ Mountie Markers/Mountie MarkersTests/Mountie_MarkersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mountie_MarkersTests.swift
+//  Mountie MarkersTests
+//
+//  Created by Blake Petipas on 5/8/25.
+//
+
+import Testing
+@testable import Mountie_Markers
+
+struct Mountie_MarkersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
